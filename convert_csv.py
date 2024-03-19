053a479282096c0b3bbc50eeeedec98b936326db
Breno Santos Cabral <brenovisk7@gmail.com>
2024-03-19 15:01:03 -0300

    upgrading convert_csv script

--- convert_csv.py
+++ convert_csv.py
@@ -1,7 +1,7 @@
 # Script criado para converter as saidas dos modelos em csv (pontos especificos)
 output_path = '/home/bcabral/'
 
-# modelos = ['BRAN',  'CGLO',  'ECCO',  'FOAM',  'GLOR12',  'GLOR4',  'HYCOM',  'ORAS',  'SODA']
+modelos = ['BRAN',  'CGLO',  'ECCO',  'FOAM',  'GLOR12',  'GLOR4',  'HYCOM',  'ORAS',  'SODA']
 # model = 'BRAN'
 
 # Importando bibliotecas
@@ -10,7 +10,7 @@ import xarray as xr
 import re
 import sys
 
-model = sys.argv[1]
+# model = sys.argv[1]
 
 # Diretorio dos modelos
 dir_model = '/data3/MOVAR/modelos/REANALISES/'
@@ -39,10 +39,10 @@ pontos_dado = {
     'Ribamar': [-2.56, -44.05]#MA
     }
 
-for year in os.listdir(f'{dir_model}{model}SSH/'):
+for year in os.listdir(f'{dir_model}{model}/SSH/'):
     if not regnumber.search(year):
         continue
-    reanalisys = xr.open_mfdataset(f'{dir_model}{model}SSH/{year}/*nc')
+    reanalisys = xr.open_mfdataset(f'{dir_model}{model}/SSH/{year}/*nc')
 
     ## precisa resolver o meio de campo aqui pra ver como faco pra pegar
     ### as variaveis
@@ -52,4 +52,34 @@ for year in os.listdir(f'{dir_model}{model}SSH/'):
         lat, lon = pontos_dado[ponto]
         ssh = reanalisys.sossheig.sel(y=lat, x=lon, method='nearest')
         ssh.to_dataframe().to_csv(f'{output_path}{model}_{ponto}_{year}.csv')
+        if model == 'BRAN':
+            ssh = ssh.rename({'yu_ocean': 'latitude', 'xu_ocean': 'longitude'})
+        elif model == 'HYCOM':
+            ssh = ssh.rename({'lat': 'latitude', 'lon': 'longitude'})
+        ssh = ssh.rename({list(ssh.keys())[0]:'ssh'})
 
+        for p in pontos_dado:
+            # aqui eu acho que vou precisar plotar cada pondo com a malha do modelo pra decidir
+            # o mais proximo
+            ponto = ssh.sel(latitude=pontos_dado[p][0], longitude=pontos_dado[p][1], method='nearest')
+            ponto = ponto['ssh']
+            ponto.to_dataframe().to_csv(f'{output_path}/{model}/{model}_{p}_{year}.csv')
+
+            ## tudo certinho! problema agr eh plotar o ponto no mapa pra ver se ta em terra ou mar
+
+
+dvars = {}
+
+for model in modelos:
+    try:
+        reanalisys = xr.open_mfdataset(f'{dir_model}{model}/SSH/{year}/*nc')
+        dvars[model] = reanalisys.coords
+    except Exception as e:
+        print(f'Erro ao abrir o modelo {model} com a excecao {e}')
+        # esperado dar erro no SODA
+        continue
+
+# mudando o nome das variaveis
+pontos[p][spao] = (curr_spaos[spao].sel(latitude=lat, longitude=lon, depth = 0,
+                               method='nearest')).rename({'latitude': 'lat', 'longitude': 'lon',
+                                                          'vo':'v', 'uo':'u'})
