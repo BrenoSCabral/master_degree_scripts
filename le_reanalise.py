9c387bcf3e2de5a1bd2f24b40e4d6be30918112a
Breno Santos Cabral <brenovisk7@gmail.com>
2024-03-20 08:44:48 -0300

    changed the get lat_lot function based on the new set_reanalisys_dims function

--- le_reanalise.py
+++ le_reanalise.py
@@ -39,7 +39,7 @@ def read_reanalisys(year, path_reanalise):
 
     return reanalisys
 
-def cut_reanalisys(reanalisys, ti, tf, lat, lon, dimensions):
+def cut_reanalisys(reanalisys, ti, tf, lat, lon):
     '''
         Makes a selection from the realanilsys domain in time and space
         :reanalisys: Dataarray
@@ -50,20 +50,22 @@ def cut_reanalisys(reanalisys, ti, tf, lat, lon, dimensions):
     '''
     variable = list(reanalisys.data_vars)[0]
 
-    if dimensions['lat'] == 'lat' and dimensions['lon'] == 'lon':
-        ssh_raw = reanalisys[variable].sel(lat=lat,
-                                    lon=lon,
-                                    method='nearest')
-    elif dimensions['lat'] == 'latitude' and dimensions['lon'] == 'longitude':
-        ssh_raw = reanalisys[variable].sel(latitude=lat,
-                                    longitude=lon,
-                                    method='nearest')
-    elif dimensions['lat'] == 'yt_ocean' and dimensions['lon'] == 'xt_ocean':
-        ssh_raw = reanalisys[variable].sel(yt_ocean=lat,
-                                    xt_ocean=lon,
-                                    method='nearest')        
-    else:
-        raise('Check latitude/longitude dimensions name.')
+    # if dimensions['lat'] == 'lat' and dimensions['lon'] == 'lon':
+    #     ssh_raw = reanalisys[variable].sel(lat=lat,
+    #                                 lon=lon,
+    #                                 method='nearest')
+    # elif dimensions['lat'] == 'latitude' and dimensions['lon'] == 'longitude':
+    #     ssh_raw = reanalisys[variable].sel(latitude=lat,
+    #                                 longitude=lon,
+    #                                 method='nearest')
+    # elif dimensions['lat'] == 'yt_ocean' and dimensions['lon'] == 'xt_ocean':
+    #     ssh_raw = reanalisys[variable].sel(yt_ocean=lat,
+    #                                 xt_ocean=lon,
+    #                                 method='nearest')        
+    # else:
+    #     raise('Check latitude/longitude dimensions name.')
+
+    ssh_raw = reanalisys[variable].sel(latitude = lat, longitude = lon)
 
 #    ???????
     try:
@@ -83,7 +85,18 @@ def get_reanalisys_dims(reanalisys):
 
         return dims
 
-def get_lat_lon(reanalisys, dimensions):
+def set_reanalisys_dims(reanalisys, name):
+    if name == 'HYCOM':
+        reanalisys = reanalisys.rename({'lat': 'latitude', 'lon': 'longitude'})
+    elif name == 'BRAN':
+        reanalisys = reanalisys.rename({'yt_ocean': 'latitude', 'xt_ocean': 'longitude'})
+
+    return reanalisys
+
+def get_lat_lon(reanalisys):
+    return (reanalisys.latitude.values, reanalisys.longitude.values)
+
+def get_lat_lon_old(reanalisys, dimensions):
     if dimensions['lat'] == 'lat' and dimensions['lon'] == 'lon':
         return(reanalisys.lat.values, reanalisys.lon.values)
     elif dimensions['lat'] == 'latitude' and dimensions['lon'] == 'longitude':
@@ -153,7 +166,8 @@ def plot_point(lat, lon, nome_modelo, nome_ponto, figs_folder, lons, lats):
             )  
     plt.text(lon - 0.005, lat - 0.005, nome_ponto,
           horizontalalignment='right', color = 'red', weight = 'bold',
-          transform=ccrs.PlateCarree())  
+          transform=ccrs.PlateCarree())
+    plt.savefig(figs_folder + f'{nome_modelo}_{nome_ponto}.png')
 
 def plot_point_broad(lat, lon, nome_modelo, nome_ponto, figs_folder, lons, lats):
     proj = ccrs.PlateCarree()
@@ -209,9 +223,11 @@ def roda_tudo(year, lat, lon, model_name, point_name, path_reanalise, figs_folde
     # if model_name == 'BRAN':
     #     dimensions = {'time': 'Time', 'lat' : 'yt_ocean', 'lon' : 'xt_ocean'}
     # else:
-    dimensions = get_reanalisys_dims(model_name)
+    # dimensions = get_reanalisys_dims(model_name)
+    reanalisys = set_reanalisys_dims(reanalisys, model_name)
 
-    lats, lons = get_lat_lon(reanalisys, dimensions)
+    lats, lons = get_lat_lon(reanalisys)
+
     plot_domain(lats, lons, model_name, figs_folder)
     plot_point(lat, lon, model_name, point_name, figs_folder, lons, lats)
     plt.close('all')
@@ -223,7 +239,7 @@ def roda_tudo(year, lat, lon, model_name, point_name, path_reanalise, figs_folde
         tf = reanalisys[dimensions['time']].values[-1]
         # tf = reanalisys.time.values[-1]
 
-    reanalisys_point = cut_reanalisys(reanalisys, ti, tf, lat, lon, dimensions)
+    reanalisys_point = cut_reanalisys(reanalisys, ti, tf, lat, lon)
     return (reanalisys_point)
 
 
