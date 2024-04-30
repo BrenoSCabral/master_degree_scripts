3d1d0a1191c6aeb6b578d1f0930c437c096ba0fb
Breno Santos Cabral <brenovisk7@gmail.com>
2024-04-29 23:08:18 -0300

    added latlon to be able to plot all series and added study to see series in the nne part

--- compara_todos_dados.py
+++ compara_todos_dados.py
@@ -5,7 +5,7 @@ import os
 
 # definindo funcao que plota o periodo
 
-def plot_series(serie):
+def plot_series(serie, figname='todas_series', xlim=False):
     '''
     Plota as séries temporais de uma lista de séries temporais.
     A serie tem que estar no formato [data_inicio, data_fim, lat]
@@ -17,7 +17,10 @@ def plot_series(serie):
         # ax.text(data_inicio, i, nome, ha='right', va='center')
 
     # Definir os rótulos dos eixos
-    plt.xlim([pd.Timestamp("19450101"), pd.Timestamp("20250101")])
+    if xlim:
+        plt.xlim(xlim)
+    else:
+        plt.xlim([pd.Timestamp("19450101"), pd.Timestamp("20250101")])
     ax.set_xlabel('Data')
 
     # Definir o título do gráfico
@@ -30,7 +33,7 @@ def plot_series(serie):
 
     # Exibir o gráfico
     plt.tight_layout()
-    plt.savefig('/Users/breno/Documents/Mestrado/estudos_dados/todas_series.png')
+    plt.savefig('/Users/breno/Documents/Mestrado/estudos_dados/' + figname + '.png')
 
 # pegando os dados da marinha:
 import plota_dados_marinha as marinha
@@ -51,12 +54,59 @@ for i in os.listdir(path):
     df = df.sort_index()
 
     lat_lon = marinha.get_lat_lon(i)    
-    serie_marinha.append((df.index[0], df.index[-1], (df.index[-1] - df.index[0]).days, lat_lon[0])) 
+    serie_marinha.append((df.index[0], df.index[-1], i, lat_lon[0])) 
 
 # pegando os dados do gloss:
 import plota_series_pontos as gloss
 path = '/Volumes/BRENO_HD/GLOSS/'
 serie_gloss = gloss.export_serie(path)
 
-
 plot_series(serie_marinha + serie_gloss)
+
+# NNE atual
+serie_total = serie_marinha + serie_gloss
+nnea = []
+for i in serie_total:
+    if i[-1]>-20 and i[1] > pd.to_datetime('2000-01-01 00:00:00'):
+        nnea.append(i)
+
+plot_series(nnea, 'nne_dps_2000', [pd.Timestamp("20000101"), pd.Timestamp("20250101")])
+
+
+#     import matplotlib.pyplot as plt
+#     import matplotlib.dates as mdates
+# def tick_ano_gpt(serie, figname='todas_series', xlim=False):
+
+#     fig, ax = plt.subplots()
+
+#     # Plotar as linhas horizontais para cada item da lista
+#     for i in serie:
+#         ax.hlines(y=i[-1], xmin=i[0], xmax=i[1], color='blue')
+#         # ax.text(data_inicio, i, nome, ha='right', va='center')
+
+#     # Definir os rótulos dos eixos
+#     if xlim:
+#         plt.xlim(xlim)
+#     else:
+#         plt.xlim([pd.Timestamp("19450101"), pd.Timestamp("20250101")])
+#     ax.set_xlabel('Data')
+
+#     # Definir o título do gráfico
+#     ax.set_title('Período de Tempo todas as séries obtidas')
+
+#     # Configurar o espaçamento dos ticks no eixo x para cada ano
+#     years = mdates.YearLocator()
+#     ax.xaxis.set_major_locator(years)
+
+#     # Formatador para exibir os anos como 'YYYY'
+#     year_format = mdates.DateFormatter('%Y')
+#     ax.xaxis.set_major_formatter(year_format)
+
+#     # Rotacionar os rótulos do eixo x para melhorar a legibilidade
+#     plt.xticks(rotation=45)
+#     plt.grid()
+#     plt.ylabel('Latitude')
+
+#     # Exibir o gráfico
+#     plt.tight_layout()
+#     plt.savefig('/Users/breno/Documents/Mestrado/estudos_dados/' + figname + '.png')
