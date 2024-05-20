a63692f9ee9d8ef52b2b6007588fed82c4d567c3
Breno Santos Cabral <brenovisk7@gmail.com>
2024-05-20 18:21:49 -0300

    finishing script that studies the distribution of all data gathered

--- distribuicao_dados.py
+++ distribuicao_dados.py
@@ -7,9 +7,10 @@ from plota_dados_marinha import get_lat_lon, nome_estacao
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+from matplotlib.lines import Line2D
 import matplotlib.ticker as mticker
 from sys import path
-
+import csv
 
 def find_point(name):
     pontos = {
@@ -112,6 +113,7 @@ def read_gloss(path, name):
     serie = serie.mask(serie < -1000)
     return serie
 
+
 def read_marinha(name):
     path = f'/Users/breno/Downloads/Dados/{name}'
     
@@ -135,12 +137,13 @@ def read_marinha(name):
 
     return df
 
+
 def get_simcosta_row(n, name):
-    df = pd.read_csv(f'/Users/breno/Documents/Mestrado/dados/simcosta/{name}', skiprows=n).dropna()
+    df = pd.read_csv(f'/Users/breno/Documents/Mestrado/dados/simcosta/{name}', skiprows=n)
     return df
 
+
 def read_simcosta(name):
-    path = f'/Users/breno/Documents/Mestrado/dados/simcosta/{name}'
     n = 10
     while n < 35:
         try:
@@ -151,10 +154,42 @@ def read_simcosta(name):
 
         except Exception as e:
             n += 1
-            
+
+    d_index = []
+    for i in range(len(df)):
+        ano = df['YEAR'][i]
+        mes = df['MONTH'][i]
+        dia = df['DAY'][i]
+        hora = df ['HOUR'][i]
+        minu = df['MINUTE'][i]
+        seg = df['SECOND'][i]
+        date = pd.to_datetime(f'{ano}-{mes}-{dia} {hora}:{minu}:{seg}')
+        d_index.append(date)
+
+    df['data'] = d_index
+    df.set_index('data', inplace=True)
+    df.drop(columns=['YEAR', 'MONTH', 'DAY', 'HOUR', 'MINUTE', 'SECOND'], inplace=True)
+
+
+    df = df.sort_index()
+
+    # pegalatlon
+    with open(f'/Users/breno/Documents/Mestrado/dados/simcosta/{name}', 'r') as arquivo:
+        # Cria um leitor CSV
+        leitor_csv = csv.reader(arquivo)
+        
+        next(leitor_csv)
+        # Lê apenas a primeira linha
+        lat = next(leitor_csv)
+        lon = next(leitor_csv)
+        
+    df['lat'] = float(lat[0][12:])
+    df['lon'] = float(lon[0][12:])
+
     return df
 
-def plota_pontos(pts):
+
+def plota_pontos(pts, namefile):
     coord = ccrs.PlateCarree()
 
     fig = plt.figure(figsize=(6, 6))
@@ -164,17 +199,28 @@ def plota_pontos(pts):
     ax.add_feature(cfeature.LAND, facecolor='lightgray')
     ax.add_feature(cfeature.BORDERS, zorder=10)
     ax.add_feature(cfeature.COASTLINE, zorder=10)
-    ax.set_extent([-55, -30, 5, -30], crs=ccrs.PlateCarree())
+    ax.set_extent([-60, -30, 5, -40], crs=ccrs.PlateCarree())
 
     # ax.set_extent([-40, 10, -50, -30], crs=coord)
 
     for point in pts:
+        marker = 'P'
+        if point[2] == 's':
+            color = 'royalblue'
+        elif point[2] == 'm':
+            color = 'lime'
+        elif point[2] == '1':
+            color = 'r'
+            marker = 'o'
+        elif point[2] == '2':
+            color = 'yellow'
+            
         plt.plot(point[1], point[0],
-                color='red', linewidth=2, marker='P',
+                color=color, linewidth=2, marker=marker,
                 transform=ccrs.PlateCarree()
                 )  
     lons = np.arange(-70, -20, 5)
-    lats = np.arange(-25, 10, 5)
+    lats = np.arange(-35, 10, 5)
     gl = ax.gridlines(crs=ccrs.PlateCarree(), linewidth=.5, color='black', alpha=0.5, linestyle='--', draw_labels=True)
     gl.xlabels_top = False
     gl.ylabels_left = False
@@ -185,30 +231,180 @@ def plota_pontos(pts):
     gl.xformatter = LONGITUDE_FORMATTER
     gl.yformatter = LATITUDE_FORMATTER
 
+    legend_elements = [
+        Line2D([0],[0], color = 'royalblue', label='SiMCosta', marker ='P', markerfacecolor='royalblue',
+               markersize=15, linestyle='None'),
+        Line2D([0],[0], color = 'lime', label='Marinha', marker ='P', markerfacecolor='lime',
+               markersize=15, linestyle='None'),
+        Line2D([0],[0], color = 'yellow', label='GOOS', marker ='P', markerfacecolor='yellow',
+               markersize=15, linestyle='None'),
+        Line2D([0],[0], color = 'r', label='GLOSS', marker ='o', markerfacecolor='r',
+               markersize=15, linestyle='None')                
+    ]
+
+    ax.legend(handles = legend_elements, loc='lower right')
+
     plt.tight_layout()
-    plt.savefig(f'/Users/breno/Documents/Mestrado/dados/nne/mapa_estacoes.png')
+    plt.savefig(f'/Users/breno/Documents/Mestrado/dados/estudo/pontos_{namefile}.png')
 
-files = {}
 
-for i in os.listdir('/Volumes/BRENO_HD/GLOSS'):
-    if i[-3:] != 'csv' or i == 'PedroPaulo_rocks_UHSLC.csv' or i[0] =='.':
-        continue
-    files[i+'g1'] = read_gloss('/Users/breno/Documents/Mestrado/dados/gloos/'+ i, i)
+def plota_series(series):
+    fig, ax = plt.subplots(figsize=(10, 15))
+    for serie in series:
+        datas = serie[3]
+        lat = serie[0]
+        origem = serie[2]
 
-for j in os.listdir('/Users/breno/Documents/Mestrado/dados/gloos/goos'):
-    files[j+'g2'] = read_gloss('/Users/breno/Documents/Mestrado/dados/gloos/goos/'+ j, j)
+        if datas[-1] < pd.Timestamp("19950101"):
+            continue
+        elif datas[0] < pd.Timestamp("19950101"):
+            data_i = pd.Timestamp("19950101")
+        else:
+            data_i = datas[0]
 
-# resolucao diaria
-# for k in os.listdir('/Users/breno/Documents/Mestrado/dados/gloos/havai'):
-#     files[k+'g3'] = read_gloss('/Users/breno/Documents/Mestrado/dados/gloos/havai/'+ k, k)
 
-for l in os.listdir('/Users/breno/Downloads/Dados'):
-    files[nome_estacao(l)[:-1]+'m'] = read_marinha(l)
+        if origem == 's':
+            color = 'royalblue'
+        elif origem == 'm':
+            color = 'lime'
+        elif origem == '1':
+            color = 'r'
+        elif origem == '2':
+            color = 'yellow'
 
-for m in os.listdir('/Users/breno/Documents/Mestrado/dados/simcosta'):
-    if m[-3:] != 'csv':
-        continue
-        # imbituba
-        # guaratuba
-    files[m+'s'] = read_simcosta(m)
 
+
+        ax.hlines(y=lat, xmin=data_i, xmax=datas[-1], color=color)
+        # ax.text(data_i, lat, nome, ha='right', va='center', size='small')
+    
+    plt.xlim([pd.Timestamp("19960101"), pd.Timestamp("20250101")])
+
+    legend_elements = [
+        Line2D([0],[0], color = 'royalblue', label='SiMCosta', markerfacecolor='royalblue', markersize=15),
+        Line2D([0],[0], color = 'lime', label='Marinha', markerfacecolor='lime', markersize=15),
+        Line2D([0],[0], color = 'yellow', label='GOOS', markerfacecolor='yellow', markersize=15),
+        Line2D([0],[0], color = 'r', label='GLOSS', markerfacecolor='r', markersize=15)                
+    ]
+
+    ax.legend(handles = legend_elements, loc='upper left')
+    plt.grid()
+
+    plt.tight_layout()
+    plt.savefig(f'/Users/breno/Documents/Mestrado/dados/estudo/abrangencia_series.png')
+
+
+def recorta_infos(infos, t0, tf=pd.Timestamp("20250101")):
+    new_infos = []
+    for info in infos:
+        data_ini = info[3][0]
+        data_fim = info[3][-1]
+        if  data_ini > tf or data_fim < t0:
+            continue
+        else:
+            new_infos.append(info)
+
+    return new_infos
+
+
+def plota_recorte(recorte, t0, tf):
+    for i in recorte:
+        if i[2] == 's':
+            color = 'royalblue'
+        elif i[2] == 'm':
+            color = 'lime'
+        elif i[2] == '1':
+            color = 'r'
+        elif i[2] == '2':
+            color = 'yellow'
+        plt.plot(i[3], [i[0]]*len(i[3]), color=color)
+
+    plt.xlim(t0, tf)
+    # plt.legend(loc='lower left', ncol=2)
+    # plt.legend(bbox_to_anchor=(1.05, 0.5))
+    plt.grid(axis='x')
+    plt.savefig(f'/Users/breno/Documents/Mestrado/dados/estudo/abrangencia_{t0.year}_{tf.year}.png')
+
+
+def main():
+    files = {}
+
+    for i in os.listdir('/Volumes/BRENO_HD/GLOSS'):
+        if i[-3:] != 'csv' or i == 'PedroPaulo_rocks_UHSLC.csv' or i[0] =='.':
+            continue
+        files[i+'g1'] = read_gloss('/Volumes/BRENO_HD/GLOSS/'+ i, i)
+
+    for j in os.listdir('/Users/breno/Documents/Mestrado/dados/gloos/goos'):
+        files[j+'g2'] = read_gloss('/Users/breno/Documents/Mestrado/dados/gloos/goos/'+ j, j)
+
+    # resolucao diaria
+    # for k in os.listdir('/Users/breno/Documents/Mestrado/dados/gloos/havai'):
+    #     files[k+'g3'] = read_gloss('/Users/breno/Documents/Mestrado/dados/gloos/havai/'+ k, k)
+
+    for l in os.listdir('/Users/breno/Downloads/Dados'):
+        files[nome_estacao(l)[:-1]+'m'] = read_marinha(l)
+
+    for m in os.listdir('/Users/breno/Documents/Mestrado/dados/simcosta'):
+        if m[-3:] != 'csv':
+            continue
+            # imbituba
+            # guaratuba
+        files[m+'s'] = read_simcosta(m)
+
+
+    infos = []
+    pts = []
+    for file in files:
+        d = files[file]
+        infos.append((d['lat'][0], d['lon'][0], file[-1], d.index, file))
+        # infos.append((d.index, d['lat'][0], file.split('.')[0], file[-1]))
+        # pts.append([d['lat'][0], d['lon'][0], file[-1]])
+
+    plota_pontos(infos, 'total')
+
+    dps95 = recorta_infos(infos, pd.Timestamp("19950101"))
+    plota_pontos(dps95, '_pos1995')
+
+    e0910 = recorta_infos(infos, pd.Timestamp("20090101"),  pd.Timestamp("20100101"))
+    plota_pontos(e0910, '_2009_2010')
+
+    e1415 = recorta_infos(infos, pd.Timestamp("20140101"),  pd.Timestamp("20150101"))
+    plota_pontos(e1415, '_2014_2015')
+
+    e1516 = recorta_infos(infos, pd.Timestamp("20150101"),  pd.Timestamp("20160101"))
+    plota_pontos(e1516, '_2015_2016')
+    # -12,97 nao usar salvador_glossbrasil.csvg1 -> salvador2.csvg1 e salvador_2004_2015.csvg1 sao iguais (nesse intervalo)
+    # -> salvador2.csvg2/'Salvador_glossbrasil.csvg1' tem dado de 5 em 5 min e eh meio diferente a forma que o dado é mostrado 
+    # -> 'CAPITANIA DE SALVADORm' eh parecido c os dois acima, mas com resultado horario e valores absolutos diferentes
+    # -23,5 -> ['ubatuba.csvg1', 'Ubatuba_gloss.csvg1', 'ubatuba.csvg2'] -> os 3 sao iguais
+    # -25,02 -> qqr cananeia serve
+    # -3,72 -> os dois fortaleza tao diferente e tem resolucao horaria diferente. n sei oq usar.
+
+    e1920 = recorta_infos(infos, pd.Timestamp("20190101"),  pd.Timestamp("20200101"))
+    plota_pontos(e1920, '_2019_2020')
+
+    e2224 = recorta_infos(infos, pd.Timestamp("20220101"),  pd.Timestamp("20240101"))
+    plota_pontos(e2224, '_2022_2024')
+
+
+def test_series(serie, lat):
+    j = []
+    for i in serie:
+        if round(i[0], 1) == lat:
+            j.append(i[-1])
+
+    for i in range(len(j)):
+        if i == 0:
+            continue
+        print((files[j[i]]['ssh']['2015-01-01':'2016-01-01'] != files[j[i-1]]['ssh']['2015-01-01':'2016-01-01']).sum())
+
+'''
+vou usar a serie 1516 e os seguintes arquivos ao longo da costa:
+-0.06 -> Santana.csvg1
+-3.72 -> Fortaleza.csvg1
+-12.97 -> salvador2.csvg1
+-22.23 -> macae.csvg2
+-22.9 -> ilha_fiscal.csvg1
+-23.5 -> ubatuba.csvg1
+-25.02 -> cananeia.csvg2
+-26.23 -> Porto de são francisco do sul (marinha)
+'''
