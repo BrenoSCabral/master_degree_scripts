496a2823079f4aeb63c8c5b664597e100226a24e
brenoscabral <brenovisk7@gmail.com>
2024-09-05 16:28:49 -0300

    adding depth and GLOR12 to reanming curr dims

--- read_reanalisys.py
+++ read_reanalisys.py
@@ -15,7 +15,8 @@ def set_reanalisys_dims(reanalisys, name, lat_name = 'latitude', lon_name = 'lon
 def get_lat_lon(reanalisys):
     return (reanalisys.latitude.values, reanalisys.longitude.values)
 
-def set_reanalisys_curr_dims(reanalisys, name, lat_name = 'latitude', lon_name = 'longitude', time_name ='time', v_name = 'v', u_name = 'u'):
+def set_reanalisys_curr_dims(reanalisys, name, lat_name = 'latitude', lon_name = 'longitude', time_name ='time',
+                                 v_name = 'v', u_name = 'u', depth_name='depth'):
     if name == 'CGLO':
         reanalisys = reanalisys.rename({'uo_cglo': u_name, 'vo_cglo': v_name})
     elif name == 'ORAS':
@@ -24,10 +25,12 @@ def set_reanalisys_curr_dims(reanalisys, name, lat_name = 'latitude', lon_name =
         reanalisys = reanalisys.rename({'uo_foam': u_name, 'vo_foam': v_name})
     elif name == 'GLOR4':
         reanalisys = reanalisys.rename({'uo_glor':u_name, 'vo_glor':v_name})
+    elif name == 'GLOR12':
+        reanalisys = reanalisys.rename({'uo':u_name, 'vo':v_name})
     elif name == 'HYCOM':
         reanalisys = reanalisys.rename({'lat': lat_name, 'lon': lon_name, 'water_u':u_name, 'water_v':v_name})
     elif name == 'BRAN':
-        reanalisys = reanalisys.rename({'yt_ocean': lat_name, 'xt_ocean': lon_name, 'Time':time_name})
+        reanalisys = reanalisys.rename({'st_ocean':depth_name, 'yt_ocean': lat_name, 'xt_ocean': lon_name, 'Time':time_name})
 
 
     return reanalisys
