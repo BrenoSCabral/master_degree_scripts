913a21fc3a851494e84f13893e2b1950e35e2ff4
Breno Santos Cabral <brenovisk7@gmail.com>
2024-09-05 11:24:23 -0300

    creating script

---
+++ current_analysis/get_curr.py
@@ -0,0 +1,178 @@
+# arquivo feito pra pegar os resultados de corrente pra reanalise e filtra-los
+import os
+import xarray as xr
+import numpy as np
+from matplotlib import pyplot as plt
+import sys
+import pandas as pd
+import datetime
+
+
+import cartopy
+import cartopy.crs as ccrs
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+import cartopy.feature as cfeature
+import matplotlib.ticker as mticker
+import matplotlib.dates as mdates
+
+
+# my files
+from read_reanalisys import set_reanalisys_curr_dims
+import filtro
+sys.path.append(
+    'old'
+)
+sys.path.append(
+    'dynamical_analysis'
+)
+import plot_hovmoller as ph
+import stats
+import general_plots as gplots
+
+import matplotlib
+matplotlib.use('TkAgg')
+
+model_path = '/data3/MOVAR/modelos/REANALISES/'
+# model_path = '/Users/breno/model_data/'
+fig_folder = '/home/bcabral/mestrado/fig/isobaths_50/'
+
+
+models =  ['BRAN', 'CGLO', 'FOAM', 'GLOR12', 'GLOR4', 'HYCOM', 'ORAS']
+
+def get_reanalisys(lat, lon, model, di, df):
+    reanal = {}
+    years = list(set([di.year, df.year]))
+    for year in years:
+        # reanal[year] = set_reanalisys_dims(xr.open_mfdataset(model_path + model + '/*.nc')
+        #                                    , model)        
+        reanal[year] = set_reanalisys_curr_dims(xr.open_mfdataset(model_path + model + '/UV/' + str(year)  + '/*.nc')
+                                            , model)
+        
+    reanalisys = xr.concat(list(reanal.values()), dim="time")
+    model_series = reanalisys.sel(latitude=lat, longitude=lon, method='nearest')
+    model_series = model_series.sel(time=slice(di, df))
+
+    mod_u = model_series['u'].values
+    mod_v = model_series['v'].values
+
+    # importante fazer aqui primeiro a conversao desse cara pra direcao along shore
+    mod_int = np.sqrt(mod_u **2 + mod_v **2) 
+    # nao tava dando problema entao n ha necessidade de fazer assim
+    # mod_ssh_to_filt = np.concatenate((np.full(len(mod_ssh)//2, mod_ssh.mean()), mod_ssh))
+    # mod_time = pd.date_range(end=model_series['time'].values[-1], periods=len(mod_ssh_to_filt), freq='D')
+    mod_time = model_series['time'].values
+    mod_band = filtro.filtra_dados(mod_ssh, mod_time, 'band', modelo = True)
+
+    return reanalisys, mod_ssh, mod_band, mod_time
+
+
+def get_points():
+    pts = {'lon': {
+  0: -51.74,
+  1: -51.21,
+  2: -50.42,
+  3: -49.675,
+  4: -49.056746,
+  5: -48.460366,
+  6: -48.24693,
+  7: -47.875,
+  8: -46.825,
+  9: -45.375,
+  10: -42.541667,
+  11: -40.425,
+  12: -40.281667,
+  13: -39.862634,
+  14: -38.908333,
+  15: -37.358333,
+  16: -38.690000,
+  17: -37.975,
+  18: -38.843861,
+  19: -38.850855,
+  20: -38.441667,
+  21: -37.477888,
+  22: -36.918087,
+  23: -35.753296,
+  24: -34.8866,
+  25: -34.550997,
+  26: -34.555217,
+  27: -34.958333,
+  28: -35.099074,
+  29: -37.512975,
+  30: -38.877564,
+  31: -42.870000,
+  32: -43.604459,
+  33: -44.641667,
+  34: -46.941667,
+  35: -48.375,
+  36: -49.508333},
+ 'lat': {0: -33.0,
+  1: -32.0,
+  2: -31.0,
+  3: -30.003125,
+  4: -29.008333,
+  5: -28.008333,
+  6: -27.008333,
+  7: -26.008333,
+  8: -25.00303,
+  9: -24.000177,
+  10: -23.000088,
+  11: -22.00119,
+  12: -21.008333,
+  13: -20.008333,
+  14: -19.004932,
+  15: -18.008333,
+  16: -17.00718,
+  17: -16.006764,
+  18: -15.008333,
+  19: -14.008333,
+  20: -13.002109,
+  21: -12.008333,
+  22: -11.008333,
+  23: -10.008333,
+  24: -9.0083333,
+  25: -8.0083333,
+  26: -7.0083333,
+  27: -6.0024336,
+  28: -5.0083333,
+  29: -4.0083333,
+  30: -3.0083333,
+  31: -2.0009244,
+  32: -1.0083333,
+  33: -0.0027777778,
+  34: 1.0029762,
+  35: 2.0008772,
+  36: 3.0053483}}
+    return pd.DataFrame(pts)
+
+
+def collect_ssh_data(pts, di, df, model):
+    ssh_data = []
+    lats = []
+    times = []
+    
+    for index, row in pts.iterrows():
+        lat = row['lat']
+        lon = row['lon']
+        _, reanalisys, fil_reanalisys, filt_time = get_reanalisys(lat, lon, model, di, df)
+        
+        # Armazenar latitude, tempo e dados de SSH
+        lats.extend([lat] * len(filt_time))
+        times.extend(filt_time)
+        ssh_data.extend(fil_reanalisys)
+    
+    # Criar DataFrame para os dados
+    df_ssh = pd.DataFrame({
+        'time': times,
+        'lat': lats,
+        'ssh': ssh_data
+    })
+    
+    return df_ssh
+
+
+year = 2015
+for model in models:
+    reanal = xr.open_mfdataset(model_path + model + '/UV/' + str(year)  + '/*.nc')
+    print('______________________________')
+    print(model, list(reanal.keys()))
+
