012296a5f2bcef79191fbe6d379c7d4d87d63ab0
Breno Santos Cabral <brenovisk7@gmail.com>
2024-09-23 23:13:35 -0300

    creating file to deal with merged series analisys

---
+++ to_run/merged_series.py
@@ -0,0 +1,4 @@
+# Arquivo criado pra fazer a analise das series concatenadas
+
+# -> isso inclui a analise de distribuição de correlação bem como a extração de métricas estatísticas e confecção dos diagramas de taylor-
+# -> aqui nao cabe fazer os espectros, acho melhor fazer um outro arquivo pra isso.
